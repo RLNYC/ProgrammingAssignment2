605554cac7280fdf530d4a5a76d3b7e317e6cf84
RLNYC <ryan.liang@macaulay.cuny.edu>
2014-12-19 20:22:58 -0500

    complete codes for cache matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R function is able to cache potentially time-consuming calculation of
+##inverse of matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat_inv <- NULL  ##Create a place holder for matrix inverse
+  
+  ##used for debugging purpose
+  set <- function(y){
+    x <<- y
+    mat_inv <<- NULL
+  }
+  
+  ##store the matrix in get
+  get <- function()x
+  
+  ##assign the inverse of matrix from cacheSolve function for the first time calculating inverse
+  setinverse <- function(inverse) mat_inv <<- inverse
+  
+  ##store matrix inverse
+  getinverse <- function() mat_inv
+  
+  ##create a list of internal functions(methods) so a calling function knows how to access these method
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse )
+  
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned 
+##by makeCacheMatrix above. If the inverse has already been calculated 
+##(and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ##call getinverse method in the makeCacheMatrix
+  mat_inv <- x$getinverse()
+  
+  ##if mat_inv is not empty,then return its stored value
+  if(!is.null(mat_inv)){
+    message("getting cached data")
+    return(mat_inv)
+  }
+  
+  ##when a matrix is passed through for the first time
+  data <- x$get()  ##obtain the matrix from makeCacheMatrix
+  mat_inv <- solve(data, ...) ##Calculate inverse of matrix
+  x$setinverse(mat_inv) ##pass the matrix inverse to makeCacheMatrix
+  mat_inv ## Return a matrix that is the inverse of 'x'
+  
+  
 }
